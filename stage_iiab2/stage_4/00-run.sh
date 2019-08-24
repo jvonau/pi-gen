cce4d5de2190a46eeab0602ad1f2a2809b79eb62
Jerry Vonau <jvonau3@gmail.com>
2019-08-24 12:09:46 -0500

    clone repos & install ansible
    add git for stage 2
    
    for 1-prep
    
    scripts/ansible dependency missing
    
    iptables-persistent pops up a dialog box during install let 1-prep deal with it

---
+++ stage_iiab2/stage_4/00-run.sh
@@ -0,0 +1,14 @@
+on_chroot << EOF
+mkdir -p /etc/iiab
+cp /opt/iiab/iiab-factory/local_vars_base.yml /etc/iiab/local_vars.yml
+cd /opt/iiab/iiab
+./runrole 1-prep
+./runrole 2-common
+./runrole httpd
+./runrole kiwix
+./runrole kalite
+rm /etc/iiab/local_vars.yml
+git checkout master
+cd /opt/iiab/iiab-factory
+git checkout master
+EOF
