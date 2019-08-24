4d26b9f360c89bbf8e4b6ffa2e5e875dc9517722
Jerry Vonau <jvonau3@gmail.com>
2019-08-24 12:09:46 -0500

    clone repos & install ansible
    add git for stage 2
    
    for 1-prep
    
    scripts/ansible dependency missing
    
    iptables-persistent pops up a dialog box during install let 1-prep deal with it

---
+++ stage_iiab2/stage_0/00-run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+mkdir -p ${ROOTFS_DIR}/opt/iiab
+
+on_chroot << EOF
+
+cd /opt/iiab/
+git clone https://github.com/iiab/iiab-factory # --depth 1
+cd /opt/iiab/iiab-factory
+git checkout -b jv-pi-gen
+git pull https://github.com/jvonau/iiab-factory.git pi-gen
+ln -sf /opt/iiab/iiab-factory/iiab /usr/sbin/iiab
+
+cd /opt/iiab/
+git clone https://github.com/iiab/iiab --depth 10
+cd /opt/iiab/iiab
+git checkout -b jv-pi-gen
+git pull https://github.com/jvonau/iiab.git pi-gen
+
+cd /opt/iiab/
+git clone https://github.com/iiab/iiab-admin-console --depth 1
+
+cd /opt/iiab/iiab/scripts
+./ansible
+EOF
