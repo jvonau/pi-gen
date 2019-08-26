bfae53b9a554d91cf532406c81dcaac733822619
Jerry Vonau <jvonau3@gmail.com>
2019-08-26 13:05:41 -0500

    try iiab-install in chroot

---
+++ stage_iiab3/iiab/00-run.sh
@@ -0,0 +1,19 @@
+on_chroot << EOF
+cp /opt/iiab/iiab/vars/local_vars_min.yml /etc/iiab/local_vars.yml
+echo "installing: True" >> /etc/iiab/local_vars.yml
+cd /opt/iiab/iiab-factory
+git checkout jv-pi-gen
+git pull https://github.com/jvonau/iiab-factory.git pi-gen
+cd /opt/iiab/iiab
+git checkout jv-pi-gen
+git pull https://github.com/jvonau/iiab.git pi-gen
+./iiab-install
+git checkout master
+cd /opt/iiab/iiab-factory
+git checkout master
+cd /opt/iiab/iiab-admin-console
+./install
+touch /opt/iiab/iiab-factory/flags/iiab-admin-console-complete
+rm /etc/iiab/local_vars.yml
+
+EOF
