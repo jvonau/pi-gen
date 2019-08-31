bbe817093c9cb8b6fd4f7004a9319f013d3dc998
Jerry Vonau <jvonau3@gmail.com>
2019-08-31 18:38:14 -0500

    3-base-server in place of httpd

--- stage_iiab2/stage_4/00-run.sh
+++ stage_iiab2/stage_4/00-run.sh
@@ -4,7 +4,7 @@ cp /opt/iiab/iiab-factory/local_vars_base.yml /etc/iiab/local_vars.yml
 cd /opt/iiab/iiab
 ./runrole 1-prep
 ./runrole 2-common
-./runrole httpd
+./runrole 3-base-server
 ./runrole kiwix
 ./runrole kalite
 rm /etc/iiab/local_vars.yml
