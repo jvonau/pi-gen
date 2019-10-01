7fbba5e30afeeb97efbd72f5fbef40ed56b9bb59
Jerry Vonau <jvonau3@gmail.com>
2019-10-01 03:28:08 -0500

    move download cleanup

--- export-image/04-finalise/00-run.sh
+++ export-image/04-finalise/00-run.sh
@@ -1,2 +1,2 @@
-echo "reset stage counter"
-sed -i 's/^STAGE=.*/STAGE=3/' ${ROOTFS_DIR}/etc/iiab/iiab.env
+#echo "reset stage counter"
+#sed -i 's/^STAGE=.*/STAGE=3/' ${ROOTFS_DIR}/etc/iiab/iiab.env
