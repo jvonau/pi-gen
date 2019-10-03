cd23c3d7e66e549fcae0df1b8cb0396e5ec38188
Jerry Vonau <jvonau3@gmail.com>
2019-10-03 12:53:24 -0500

    disable min build

--- stage_iiab2/stage_4/00-run.sh
+++ stage_iiab2/stage_4/00-run.sh
@@ -1,10 +1,10 @@
 on_chroot << EOF
-mkdir -p /etc/iiab
-cp /opt/iiab/iiab-factory/pi-gen/local_vars_base.yml /etc/iiab/local_vars.yml
-cd /opt/iiab/iiab
-./runrole 1-prep
-./runrole 2-common
-./runrole 3-base-server
+#mkdir -p /etc/iiab/flags
+#cp /opt/iiab/iiab-factory/pi-gen/local_vars_base.yml /etc/iiab/local_vars.yml
+#cd /opt/iiab/iiab
+#./runrole 1-prep
+#./runrole 2-common
+#./runrole 3-base-server
 #./runrole mysql
 #./runrole homepage
 #./runrole kiwix
@@ -14,5 +14,5 @@ cd /opt/iiab/iiab
 #cd /opt/iiab/iiab-admin-console
 #./install
 #touch /opt/iiab/iiab-factory/flags/iiab-admin-console-complete
-rm /etc/iiab/local_vars.yml
+#rm /etc/iiab/local_vars.yml
 EOF
