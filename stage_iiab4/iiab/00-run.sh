c0fcfdcac8881eb0854dddca6b377d54dcfe0a30
Jerry Vonau <jvonau3@gmail.com>
2019-10-06 21:16:38 -0500

    revised flag path #79

--- stage_iiab4/iiab/00-run.sh
+++ stage_iiab4/iiab/00-run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -e
 on_chroot << EOF
-mkdir -p /etc/iiab/flags
+mkdir -p /etc/iiab/install-flags
 cd /opt/iiab/iiab-factory
 git checkout jv-pi-gen
 git pull https://github.com/jvonau/iiab-factory.git pi-gen
 cp /opt/iiab/iiab-factory/pi-gen/local_vars_medium.yml /etc/iiab/local_vars.yml
 cd /opt/iiab/iiab
 git checkout master
+git pull
+git checkout release-7.0
 git pull
 git branch -D jv-pi-gen || true
 git checkout -b jv-pi-gen
@@ -17,24 +19,24 @@ git pull
 EOF
 
 on_chroot << EOF2
-if [ ! -f /opt/iiab/iiab-factory/flags/iiab-admin-console-complete ]; then
+if [ ! -f /etc/iiab/install-flags/iiab-admin-console-complete ]; then
     cd /opt/iiab/iiab-admin-console
     ./install
-    touch /etc/iiab/flags/iiab-admin-console-complete
+    touch /etc/iiab/install-flags/iiab-admin-console-complete
     fi
 EOF2
 
 on_chroot << EOF3
     echo -e 'Now running kalite zone'
     kalite manage generate_zone
-    touch /etc/iiab/flags/kalite-zone-complete
+    touch /etc/iiab/install-flags/kalite-zone-complete
 
     echo -e 'Now retreiving kalite en.zip'
     cd /opt/iiab/downloads
     wget http://pantry.learningequality.org/downloads/ka-lite/0.17/content/contentpacks/en.zip
     echo -e 'Now installing kalite en.zip'
     kalite manage retrievecontentpack local en en.zip
-    touch /etc/iiab/flags/kalite-en.zip-complete
+    touch /etc/iiab/install-flags/kalite-en.zip-complete
     cd /opt/iiab/iiab-factory
     git checkout master
     sed -i 's|_installed|_installed: True|' /etc/iiab/config_vars2.yml
