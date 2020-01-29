fe0f2bed2141fc80d6e03e4a2e7766d5b2b157f6
Jerry Vonau <jvonau3@gmail.com>
2020-01-29 15:00:07 -0600

    update iiab_stage2

--- stage_iiab2/stage_0/00-run.sh
+++ stage_iiab2/stage_0/00-run.sh
@@ -2,25 +2,29 @@
 mkdir -p ${ROOTFS_DIR}/opt/iiab
 
 on_chroot << EOF
+# use the stock iiab
+curl http://d.iiab.io/7.1/iiab > /usr/sbin/iiab
+chmod 0744 /usr/sbin/iiab
 
+# need the local_vars file
 cd /opt/iiab/
 git clone https://github.com/iiab/iiab-factory # --depth 1
 cd /opt/iiab/iiab-factory
 git checkout -b jv-pi-gen
 git pull https://github.com/jvonau/iiab-factory.git pi-gen
-ln -sf /opt/iiab/iiab-factory/iiab /usr/sbin/iiab
+#ln -sf /opt/iiab/iiab-factory/iiab /usr/sbin/iiab
 
 cd /opt/iiab/
-git clone https://github.com/iiab/iiab --branch release-7.0 --depth 10
+git clone https://github.com/iiab/iiab --branch master --depth 10
 cd /opt/iiab/iiab
-git checkout -b master
-git config branch.master.remote origin
-git config branch.master.merge refs/heads/master
-git checkout -b jv-pi-gen
-git pull https://github.com/jvonau/iiab.git installed2
+#git checkout -b master
+#git config branch.master.remote origin
+#git config branch.master.merge refs/heads/master
+git checkout -b target
+git pull https://github.com/jvonau/iiab.git target
 
 cd /opt/iiab/
-git clone https://github.com/iiab/iiab-admin-console --branch v0.3.7 --depth 1
+git clone https://github.com/iiab/iiab-admin-console --branch master --depth 10
 
 cd /opt/iiab/iiab/scripts
 ./ansible
