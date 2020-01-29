568e9a28155b668532a6f72475ed45343e94b4ff
Jerry Vonau <jvonau3@gmail.com>
2020-01-29 15:11:55 -0600

    update iiab_stage3

--- stage_iiab3/iiab/00-run.sh
+++ stage_iiab3/iiab/00-run.sh
@@ -6,14 +6,12 @@ git checkout jv-pi-gen
 git pull https://github.com/jvonau/iiab-factory.git pi-gen
 cp /opt/iiab/iiab-factor/pi-gen/local_vars_min.yml /etc/iiab/local_vars.yml
 cd /opt/iiab/iiab
-git checkout release-7.0
-git branch -D jv-pi-gen || true
-git checkout -b jv-pi-gen
-git pull https://github.com/jvonau/iiab.git installed2
-./iiab-install
 git checkout master
 git pull
-git checkout release-7.0
+git branch -D target || true
+git checkout -b target
+git pull https://github.com/jvonau/iiab.git target
+./iiab-install
 EOF
 on_chroot << EOF2
 #if [ ! -f /opt/iiab/iiab-factory/flags/iiab-admin-console-complete ]; then
@@ -27,4 +25,4 @@ apt-get -y dist-upgrade
 EOF2
 
 rm ${ROOTFS_DIR}/etc/iiab/local_vars.yml
-sed -i 's/^STAGE=.*/STAGE=2/' ${ROOTFS_DIR}/etc/iiab/iiab.env
+#sed -i 's/^STAGE=.*/STAGE=2/' ${ROOTFS_DIR}/etc/iiab/iiab.env
