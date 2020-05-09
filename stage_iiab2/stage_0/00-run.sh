73c7877079edbd0cc034e4c008a6fc392ce91592
Jerry Vonau <jvonau3@gmail.com>
2020-05-09 16:45:10 -0500

    move ansible pip and use apt

--- stage_iiab2/stage_0/00-run.sh
+++ stage_iiab2/stage_0/00-run.sh
@@ -7,19 +7,20 @@ curl https://raw.githubusercontent.com/iiab/iiab-factory/master/iiab > /usr/sbin
 chmod 0744 /usr/sbin/iiab
 
 cd /opt/iiab/
-git clone https://github.com/raspberrypi/rpi-eeprom
-sed -i 's/python/python3/' rpi-eeprom/rpi-eeprom-config
-rpi-eeprom/test/install
+#git clone https://github.com/raspberrypi/rpi-eeprom
+#sed -i 's/python/python3/' rpi-eeprom/rpi-eeprom-config
+#rpi-eeprom/test/install
 
-cd /opt/iiab/
 git clone https://github.com/iiab/iiab-factory --depth 1
-
-cd /opt/iiab/
 git clone https://github.com/iiab/iiab --branch master
-
-cd /opt/iiab/
 git clone https://github.com/iiab/iiab-admin-console --branch master --depth 10
 
-#cd /opt/iiab/iiab/scripts
-#./ansible
+#export DEBIAN_FRONTEND=noninteractive
+#apt -y install python3-pip python3-setuptools python3-venv virtualenv
+#python3 -m venv /usr/local/ansible
+#source /usr/local/ansible/bin/activate
+#sudo pip3 install ansible pymysql psycopg2 passlib
+#deactivate
+cd /opt/iiab/iiab/scripts
+./ansible
 EOF
--- stage_iiab2/stage_4/00-run.sh
+++ stage_iiab2/stage_4/00-run.sh
@@ -1,8 +1,9 @@
 on_chroot << EOF
-export DEBIAN_FRONTEND=noninteractive
-apt -y install python3-pip python3-setuptools python3-venv virtualenv
-python3 -m venv /usr/local/ansible
-source /usr/local/ansible/bin/activate
-sudo pip3 install ansible pymysql psycopg2 passlib
-deactivate
+echo "disabled"
+#export DEBIAN_FRONTEND=noninteractive
+#apt -y install python3-pip python3-setuptools python3-venv virtualenv
+#python3 -m venv /usr/local/ansible
+#source /usr/local/ansible/bin/activate
+#sudo pip3 install ansible pymysql psycopg2 passlib
+#deactivate
 EOF
